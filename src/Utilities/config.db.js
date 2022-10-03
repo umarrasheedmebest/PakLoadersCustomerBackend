4a75ffe34db3c2375db08e3245d8b700fa3169b8
ammaralizahid <amilzak42@gmail.com>
2022-10-03 09:01:32 -0700

    user register(activate profile)

--- src/Utilities/config.db.js
+++ src/Utilities/config.db.js
@@ -1,35 +1,27 @@
-// const mysql = require('mysql');
+const mysql = require('mysql');
 
+const conn = mysql.createPool({
 
-
-
-// const conn = mysql.createPool({
-
-//   host: "localhost",
-//   user:"root",
-//   password:"",
-//   database: "practice",
-//   port: 3306
+  host: "localhost",
+  user:"root",
+  password:"",
+  database: "pakloaders",
+  port: 3306
     
-// });
+});
 
 
-// conn.getConnection((err,connection)=>{
-//   if(err){
-//     console.error("error connection:" + err.stack)
-//     return;
-//   }
-//   if(connection){
-//     console.log("Database Connection established! ")
-//     connection.release()
-//   }
-// })
-
-
-
+conn.getConnection((err,connection)=>{
+  if(err){
+    console.error("error connection:" + err.stack)
+    return;
+  }
+  if(connection){
+    console.log("Database Connection established! ")
+    connection.release()
+  }
+})
 
 
 
-
-
-// module.exports = conn;
+module.exports = conn;
---
+++ src/Utilities/validations.js
@@ -0,0 +1,13 @@
+const joi= require('joi')
+
+
+const signUp =  joi.object({
+    
+});
+
+
+module.exports = {
+
+signUp
+
+}
