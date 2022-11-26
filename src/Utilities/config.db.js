8824e7843ac9a29be16e0b8f9aec1683e6474ef7
ammaralizahid <amilzak42@gmail.com>
2022-11-26 19:46:52 +0500

    change db

--- src/Utilities/config.db.js
+++ src/Utilities/config.db.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql');
 
-// const conn = mysql.createPool({
-//   host: "us-cdbr-east-06.cleardb.net",
-//   user:"b6812775b5b855",
-//   password:"5a8a8dbd",
-//   database: "heroku_1f8fedd230d2b92",
-//   port: 3306 
-// });
 const conn = mysql.createPool({
-  host: "localhost",
-  user:"root",
-  password:"",
-  database: "pakloaders",
+  host: "us-cdbr-east-06.cleardb.net",
+  user:"b6812775b5b855",
+  password:"5a8a8dbd",
+  database: "heroku_1f8fedd230d2b92",
   port: 3306 
 });
+// const conn = mysql.createPool({
+//   host: "localhost",
+//   user:"root",
+//   password:"",
+//   database: "pakloaders",
+//   port: 3306 
+// });
 conn.getConnection((err,connection)=>{
   if(err){
     console.error("error connection:" + err.stack)
