f92c838a4b85dddd2dd36cffa047800810b22da4
ammaralizahid <amilzak42@gmail.com>
2022-12-31 12:51:06 +0500

    db config with railway.app

--- src/Utilities/config.db.js
+++ src/Utilities/config.db.js
@@ -1,19 +1,30 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
-const conn = mysql.createPool({
-  host: "us-cdbr-east-06.cleardb.net",
-  user:"b6812775b5b855",
-  password:"5a8a8dbd",
-  database: "heroku_1f8fedd230d2b92",
-  port: 3306 
-});
+// const conn = mysql.createPool({
+//   host: "us-cdbr-east-06.cleardb.net",
+//   user:"b6812775b5b855",
+//   password:"5a8a8dbd",
+//   database: "heroku_1f8fedd230d2b92",
+//   port: 3306 
+// });
+
 // const conn = mysql.createPool({
 //   host: "localhost",
 //   user:"root",
-//   password:"",
+//   password:"", 
 //   database: "pakloaders",
 //   port: 3306 
 // });
+
+// mysql -hcontainers-us-west-69.railway.app -uroot -pEgtFRv5kLkGM0SlIXz2d --port 7654 --protocol=TCP railway
+
+const conn = mysql.createPool({
+  host: "containers-us-west-69.railway.app",
+  user:"root",
+  password:"EgtFRv5kLkGM0SlIXz2d", 
+  database: "railway",
+  port: 7654 
+});
 conn.getConnection((err,connection)=>{
   if(err){
     console.error("error connection:" + err.stack)
