67aef1393db3e26579f00ece5b84559140b2adc5
ammaralizahid <amilzak42@gmail.com>
2023-02-21 23:13:52 +0500

    sms service api key

--- src/Utilities/config.db.js
+++ src/Utilities/config.db.js
@@ -2,13 +2,13 @@ const mysql = require('mysql');
 
 // umer rasheed credentials
 
-const conn = mysql.createPool({
-  host: "sql6.freemysqlhosting.net",
-  user:"sql6590784",
-  password:"UepK2CveRe", 
-  database: "sql6590784",
-  port: 3306 
-});
+// const conn = mysql.createPool({
+//   host: "sql6.freemysqlhosting.net",
+//   user:"sql6590784",
+//   password:"UepK2CveRe", 
+//   database: "sql6590784",
+//   port: 3306 
+// });
 
 // const conn = mysql.createPool({
 //   host: "sql12.freemysqlhosting.net",
@@ -27,13 +27,13 @@ const conn = mysql.createPool({
 //   port: 3306 
 // });
 
-// const conn = mysql.createPool({
-//   host: "localhost",
-//   user:"root",
-//   password:"", 
-//   database: "pakloaders",
-//   port: 3306 
-// });
+const conn = mysql.createPool({
+  host: "localhost",
+  user:"root",
+  password:"", 
+  database: "pakloaders",
+  port: 3306 
+});
 conn.getConnection((err,connection)=>{
   if(err){
     console.error("error connection:" + err.stack)
