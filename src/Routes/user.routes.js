2a6d863636b0f0fb3da3e82027378646c34f9158
ammaralizahid <amilzak42@gmail.com>
2023-02-25 20:36:39 +0500

    changes

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const userController = require('../Controller/user.controller')
 const {verifyAccessToken} = require('../Utilities/jwt')
 
-router.delete("/delete/:id", userController.deleteUser);
-router.get("/get/:id", userController.getUser);
-router.put("/update/:id",userController.updateUser);
+router.delete("/delete/:id",verifyAccessToken, userController.deleteUser);
+router.get("/get/:id",verifyAccessToken, userController.getUser);
+router.put("/update/:id",verifyAccessToken,userController.updateUser);
 
 module.exports = router;
